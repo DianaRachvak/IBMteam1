8f5c12c4d0d9ee192a377ddc7eb63ffcc04cffe6
Diana Rachvak <dianarachvak6@gmail.com>
2018-11-02 11:07:17 -0400

    added header

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ $("#start-record-btn").on("click", function(e) {
 $("#pause-record-btn").on("click", function(e) {
   recognition.stop();
   instructions.text("Voice recognition paused.");
-  /*
+  
   if (!noteContent.length) {
     instructions.text(
       "Could not save empty note. Please add a message to your note."
@@ -120,7 +120,7 @@ $("#pause-record-btn").on("click", function(e) {
     renderNotes(getAllNotes());
     noteTextarea.val("");
     instructions.text("Note saved successfully.");
-  }*/
+  }
 });
 
 // Sync the text inside the text area with the noteContent variable.
